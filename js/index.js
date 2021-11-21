3d4c5be1535cf06d35c7d245ad32347e22770e60
hassanabd22 <76787879+hassanabd22@users.noreply.github.com>
2021-11-21 20:42:26 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const info = [
     {
-        src:'/images/image-tanya.jpg',
+        src:'images/image-tanya.jpg',
         text:`“ I’ve been interested in coding for a while but never taken the jump, until now. 
         I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
         excited about the future. ” `,
@@ -8,7 +8,7 @@ const info = [
         job:'UX Engineer'
     },
     {
-        src:'/images/image-john.jpg',
+        src:'images/image-john.jpg',
         text:`“ If you want to lay the best foundation possible I’d recommend taking this course. 
         The depth the instructors go into is incredible. I now feel so confident about 
         starting up as a professional developer. ”`,
